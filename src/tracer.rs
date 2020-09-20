5eb1d41a02709620c91d5515a1749da226b28a06
Eiji Akagi <eiji@noir.local>
2020-09-20 09:16:05 +0900

    is: tupple to member

--- src/tracer.rs
+++ src/tracer.rs
@@ -27,40 +27,39 @@ pub fn trace_photon(uc: &bool, m0: &Material, objs: &Vec<Object>, l: i32, ph: &P
   }
   
   let is = calc_intersection(&ph.ray, objs);
-  let is2 = is.unwrap();
-  let Intersection(p, n, m) = is2;
-  let d = m.diffuseness;
+  let is1 = is.unwrap();
+  let d = is1.mate.diffuseness;
   let i = russian_roulette(&[d]);
   let mut pcs = if i > 0 {
-    reflect_diff(uc, m0, objs, l, ph, &is2)
+    reflect_diff(uc, m0, objs, l, ph, &is1)
   } else {
-    reflect_spec(uc, m0, objs, l, ph, &is2)
+    reflect_spec(uc, m0, objs, l, ph, &is1)
   };
   if (*uc == false || l > 0) && d > 0.0 {
-    pcs.push(Photon::new(&ph.wl, &Ray::new(&p, &ph.ray.dir)));
+    pcs.push(Photon::new(&ph.wl, &Ray::new(&is1.pos, &ph.ray.dir)));
   }
   pcs
 }
 
 fn reflect_diff(uc: &bool, m0: &Material, objs: &Vec<Object>, l: i32, ph: &Photon, is: &Intersection) -> Vec<PhotonCache> {
-  let i = russian_roulette(&[is.2.reflectance.select_wavelength(ph.wl)]);
+  let i = russian_roulette(&[is.mate.reflectance.select_wavelength(ph.wl)]);
   if i > 0 {
-    let dr = diffuse_reflection(&is.1);
-    trace_photon(uc, m0, objs, l+1, &Photon::new(&ph.wl, &Ray::new(&is.0, &dr)))
+    let dr = diffuse_reflection(&is.nvec);
+    trace_photon(uc, m0, objs, l+1, &Photon::new(&ph.wl, &Ray::new(&is.pos, &dr)))
   } else{
     vec![]
   }
 }
 
 fn reflect_spec(uc: &bool, m0: &Material, objs: &Vec<Object>, l: i32, ph: &Photon, is: &Intersection) -> Vec<PhotonCache> {
-  let f0 = is.2.specular_refl.select_wavelength(ph.wl);
-  let (rdir, cos0) = specular_reflection(&is.1, &ph.ray.dir);
+  let f0 = is.mate.specular_refl.select_wavelength(ph.wl);
+  let (rdir, cos0) = specular_reflection(&is.nvec, &ph.ray.dir);
   let f = f0 + (1.0 - f0) * (1.0 - cos0).powf(5.0);
   let j = russian_roulette(&[f]);
   if j > 0 {
-    trace_photon(uc, m0, objs, l+1, &Photon::new(&ph.wl, &Ray::new(&is.0, &rdir)))
+    trace_photon(uc, m0, objs, l+1, &Photon::new(&ph.wl, &Ray::new(&is.pos, &rdir)))
   } else {
-    if is.2.ior.select_wavelength(ph.wl) == 0.0 {
+    if is.mate.ior.select_wavelength(ph.wl) == 0.0 {
       vec![]
     } else {
       reflect_trans(uc, m0, objs, l, ph, is, &cos0)
@@ -70,14 +69,14 @@ fn reflect_spec(uc: &bool, m0: &Material, objs: &Vec<Object>, l: i32, ph: &Photo
 
 fn reflect_trans(uc: &bool, m0: &Material, objs: &Vec<Object>, l: i32, ph: &Photon, is: &Intersection, c0: &Flt) -> Vec<PhotonCache> {
   let ior0 = m0.ior.select_wavelength(ph.wl);
-  let ior1 = is.2.ior.select_wavelength(ph.wl);
-  let (tdir, ior2) = specular_refraction(&ior0, &ior1, c0, &ph.ray.dir, &is.1);
-  let m02 = if tdir.dot(&is.1) < 0.0 {
-    is.2
+  let ior1 = is.mate.ior.select_wavelength(ph.wl);
+  let (tdir, ior2) = specular_refraction(&ior0, &ior1, c0, &ph.ray.dir, &is.nvec);
+  let m02 = if tdir.dot(&is.nvec) < 0.0 {
+    is.mate
   } else {
     M_AIR
   };
-  trace_photon(uc, &m02, objs, l+1, &Photon::new(&ph.wl, &Ray::new(&is.0, &tdir)))
+  trace_photon(uc, &m02, objs, l+1, &Photon::new(&ph.wl, &Ray::new(&is.pos, &tdir)))
 }
 
 // Photon mapping method
@@ -91,28 +90,28 @@ pub fn trace_ray(scr: &Screen, m0: &Material, l: i32, pmap: &PhotonMap, objs: &V
     return Radiance::RADIANCE0
   }
 
-  let Intersection(p, n, m) = is.unwrap();
+  let is1 = is.unwrap();
   let di = if scr.use_classic_for_direct == true {
     let mut rad = Radiance::RADIANCE0;
     for l in lgts {
-      rad = rad + get_radiance_from_light(objs, &p, &n, l);
+      rad = rad + get_radiance_from_light(objs, &is1.pos, &is1.nvec, l);
     }
     rad
   } else {
     Radiance::RADIANCE0
   };
-  let ii = estimate_radiance(&scr, &pmap, &p, &n, &m);
-  let (rdir, cos0) = specular_reflection(&n, &r.dir);
-  let df = m.diffuseness;
-  let mt = m.metalness;
-  let f = reflection_index(&m.specular_refl, &cos0);
+  let ii = estimate_radiance(&scr, &pmap, &is1);
+  let (rdir, cos0) = specular_reflection(&is1.nvec, &r.dir);
+  let df = is1.mate.diffuseness;
+  let mt = is1.mate.metalness;
+  let f = reflection_index(&is1.mate.specular_refl, &cos0);
   let f2 = -f;
-  let ior1 = m.average_ior();
+  let ior1 = is1.mate.average_ior();
   
   let si = if df == 1.0 || f == Color::BLACK {
     Radiance::RADIANCE0
   } else {
-    trace_ray(scr, m0, l+1, pmap, objs, lgts, &Ray::new(&p, &rdir))
+    trace_ray(scr, m0, l+1, pmap, objs, lgts, &Ray::new(&is1.pos, &rdir))
   };
   let ti = if f2 == Color::BLACK || ior1 == 0.0 {
     Radiance::RADIANCE0
@@ -120,10 +119,10 @@ pub fn trace_ray(scr: &Screen, m0: &Material, l: i32, pmap: &PhotonMap, objs: &V
     Radiance::RADIANCE0
   };
 
-  m.emittance * SR_HALF + df * brdf(&m, &(di + ii)) + (1.0 - df) * (f * si + (1.0 - mt) * f2 * ti)
+  is1.mate.emittance * SR_HALF + df * brdf(&is1.mate, &(di + ii)) + (1.0 - df) * (f * si + (1.0 - mt) * f2 * ti)
 }
 
-fn estimate_radiance(scr: &Screen, pmap: &PhotonMap, p: &Position3, n: &Direction3, m: &Material) -> Radiance {
+fn estimate_radiance(scr: &Screen, pmap: &PhotonMap, is: &Intersection) -> Radiance {
   Radiance::RADIANCE0
 }
 
@@ -140,36 +139,36 @@ pub fn trace_ray_classic(scr: &Screen, m0: &Material, l: i32, objs: &Vec<Object>
     return Radiance::RADIANCE0
   }
 
-  let Intersection(p, n, m) = is.unwrap();
-  let diffs = lgts.iter().map(|l| get_radiance_from_light(objs, &p, &n, &l));
+  let is1 = is.unwrap();
+  let diffs = lgts.iter().map(|l| get_radiance_from_light(objs, &is1.pos, &is1.nvec, &l));
   let mut rad_diff = Radiance::RADIANCE0;
   for d in diffs {
     rad_diff = rad_diff + d;
   }
   let ii = Radiance::RADIANCE0;
-  let (rdir, cos0) = specular_reflection(&n, &r.dir);
-  let df = m.diffuseness;
-  let mt = m.metalness;
-  let f = reflection_index(&m.specular_refl, &cos0);
+  let (rdir, cos0) = specular_reflection(&is1.nvec, &r.dir);
+  let df = is1.mate.diffuseness;
+  let mt = is1.mate.metalness;
+  let f = reflection_index(&is1.mate.specular_refl, &cos0);
   let f2 = -f;
-  let ior1 = m.average_ior();
+  let ior1 = is1.mate.average_ior();
 
   let si = if  df == 1.0 || f == Color::BLACK {
     Radiance::RADIANCE0
   } else {
-    trace_ray_classic(scr, m0, l+1, objs, lgts, &Ray::new(&p, &rdir))
+    trace_ray_classic(scr, m0, l+1, objs, lgts, &Ray::new(&is1.pos, &rdir))
   };
   let ti = if f2 == Color::BLACK || ior1 == 0.0 {
     Radiance::RADIANCE0
   } else {
     let ior0 = m0.average_ior();
-    let (tdir, ior2) = specular_refraction(&ior0, &ior1, &cos0, &r.dir, &n);
-    let m02 = if tdir.dot(&n) < 0.0 { m } else { M_AIR };
-    trace_ray_classic(scr, &m02, l+1, objs, lgts, &Ray::new(&p, &tdir))
+    let (tdir, ior2) = specular_refraction(&ior0, &ior1, &cos0, &r.dir, &is1.nvec);
+    let m02 = if tdir.dot(&is1.nvec) < 0.0 { is1.mate } else { M_AIR };
+    trace_ray_classic(scr, &m02, l+1, objs, lgts, &Ray::new(&is1.pos, &tdir))
   };
 
-  m.emittance * SR_HALF +
-    df         * brdf(&m, &(scr.ambient + rad_diff)) +
+  is1.mate.emittance * SR_HALF +
+    df         * brdf(&is1.mate, &(scr.ambient + rad_diff)) +
     (1.0 - df) * (f * si + (1.0 - mt) * f2 * ti)
 }
 
@@ -195,7 +194,7 @@ fn illuminated(os: &Vec<Object>, p: &Position3, n: &Direction3, lds: &Vec<Direct
     let lray = Ray::new(&p, &ld3);
     let is = calc_intersection(&lray, os);
     if is == None { continue; }
-    let p2 = is.unwrap().0;
+    let p2 = is.unwrap().pos;
     let sq_ldist = ld.square();
     let sq_odist = (p2 - *p).square();
     if sq_ldist - sq_odist > 0.002 { continue; }
@@ -205,7 +204,11 @@ fn illuminated(os: &Vec<Object>, p: &Position3, n: &Direction3, lds: &Vec<Direct
 }
 
 #[derive(PartialEq)]
-struct Intersection(pub Position3, pub Direction3, pub Material);
+struct Intersection {
+  pub pos: Position3,
+  pub nvec: Direction3,
+  pub mate: Material,
+}
 
 fn calc_intersection(r: &Ray, os: &Vec<Object>) -> Option<Intersection> {
   fn sorting(is1: &(Flt, Object), is2: &(Flt, Object)) -> Ordering {
@@ -243,7 +246,7 @@ fn calc_intersection(r: &Ray, os: &Vec<Object>) -> Option<Intersection> {
     if nvec == None {
       None
     } else {
-      Some(Intersection(p, nvec.unwrap(), obj.material))
+      Some(Intersection {pos: p, nvec: nvec.unwrap(), mate: obj.material})
     }
   }
 }
